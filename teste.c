1cedcbb53cffea6d43f4dc7c8f8bee2f008a57e5
Heuler Mouta <hmd187@gmail.com>
2021-08-04 16:52:21 +0000

    Inserido primeiros arquivos em linguagem C

---
+++ teste.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    float lado1, lado2;
+    printf("Por favor escreva os lados:\n");
+    scanf("%f %f", &lado1 , &lado2);
+    printf("Os valores digitados sao %f e %f\n", lado1, lado2);
+}
