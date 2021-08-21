c428379eb57679de73366b865bfe7b8ced5c3af0
Heuler <hmd187@gmail.com>
2021-08-21 15:01:20 -0300

    atualizado repositório

--- teste.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    float lado1, lado2;
-    printf("Por favor escreva os lados:\n");
-    scanf("%f %f", &lado1 , &lado2);
-    printf("Os valores digitados sao %f e %f\n", lado1, lado2);
-}
